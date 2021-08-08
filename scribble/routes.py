d64490143c2129d577b2e76a9ef68e0f3ffb9641
Zhenya Vo <sverhzad@gmial.com>
2021-08-08 20:10:49 +0300

    fix gallery, need margin-top:auto;

--- scribble/routes.py
+++ scribble/routes.py
@@ -113,41 +113,20 @@ def answer(id):
 @main.route('/gallery')
 def gallery():
     predictions = sorted(Exhibit.query.all(), key = lambda x: x.length)
-    boys, girls = [], []
+    boys, girls, more = [], [], 0
     for person in predictions:
         if person.your_size > 0:
             boys.append(person)
-        else: girls.append(person)
-    all_persons = OrderedDict()
-    if len(girls) > len(boys):
-        longest, shortest = iter(girls), iter(boys)
-        ranger = range(len(girls))
-        sex_1, sex_2 = 'girl', 'boy'
-    else:
-        longest, shortest = iter(boys), iter(girls)
-        ranger = range(len(boys))
-        sex_1, sex_2 = 'boy', 'girl'
-    for _ in ranger:
-        try:
-            long = next(longest)
-            short= next(shortest)
-            l_1, l_2 = long.length - short.length, short.length - long.length
-
-            all_persons.update({long: [sex_1, l_1]})
-            all_persons.update({short: [sex_2, l_2]})
-        except(StopIteration):
-            if len(girls) - len(boys) == 0:
-                break
-            try:
-                all_persons.update({long: [sex_1, 0]})
-            except(StopIteration):
-                break
+            more += 1
+        else:
+            girls.append(person)
+            more -= 1
+    ans = zip(boys, girls)
     if predictions:
         bigest = predictions[-1].id
         smollest = predictions[0].id
     else: bigest, smollest = None, None
-    return render_template('gallery.html', bigest = bigest, smollest = smollest, all_persons = all_persons)
-
+    return render_template('gallery.html', bigest=bigest, smollest=smollest, ans=ans)
 
 @main.route('/login', methods=['POST', 'GET'])
 def login():
