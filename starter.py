d32869779428463ee975e44bf53f1623bfe745e1
Zhenya Vo <sverhzad@gmial.com>
2021-08-09 16:19:40 +0300

    remove unused

--- starter.py
+++ starter.py
@@ -1,18 +1,12 @@
 import os
-#from flask_script import Manager, Shell
 #from flask_migrate import MigrateCommand
 from scribble import create_app, db
 from scribble.models import Owner, Exhibit
 from flask.cli import FlaskGroup
 
 app = create_app()
-app.config['SECRET_KEY'] = 'mysecret'
 
-'''
-app.config['SECRET_KEY'] = 'mysecret'
 
-app = create_app(os.getenv('FLASK_ENV') or
-        'config.DevelopmentConfig')'''
 cli = FlaskGroup(app)
 '''
 @app.shell_context_processor
@@ -21,12 +15,5 @@ def make_shell_context():
 '''
 
 
-'''
-def make_shell_context():
-    return dict(app=app, db=db, Owner=Owner, Exhibit=Exhibit)
-'''
-
-
 if __name__ == '__main__':
-    #manager.run()
     cli()
